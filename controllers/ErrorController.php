756eb2bdd8f8d0cd3e2a88961a57d361a07f7495
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-06-09 17:15:32 +0300

    ended error controller

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -2,9 +2,12 @@
 namespace controllers;
 use core\Config;
 use core\Controller;
+use core\Core;
+
 class ErrorController extends Controller
 {
     public function actionError(array $code):array{
+        Core::getInstance()->isError = true;
         $num=$this->setRandomError();
         if($code[0]==403){
             $code []=Config::getInstance()->error403;
